2930022dbf532b523d0b1dc5cdac82c167186af0
Sergey Starzhinskiy <sstarzh@gmail.com>
2022-05-12 12:24:11 -0400

    Updated detectorv2 script

--- detectorv2.py
+++ detectorv2.py
@@ -13,11 +13,6 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from socketserver import ThreadingMixIn
 import threading
 
-hostname = 'arcadia.f5ase.net'
-origin = 'arcadia.f5ase.net'
-load_my_model = 'detector_model_distilbert'
-serverPort = 8081
-
 
 def set_header():
     headers = {
@@ -25,13 +20,31 @@ def set_header():
     }
 
     return headers
+def parse_args(argv=sys.argv[1:]):
+    parser = argparse.ArgumentParser(description='Proxy HTTP requests')
+    parser.add_argument('--port', dest='port', type=int, default=8081,
+                        help='serve HTTP requests on specified port (default: random)')
+    parser.add_argument('--origin', dest='origin', type=str, default='arcadia.f5ase.net',
+                        help='hostname of the origin (default: arcadia.f5ase.net)')
+    parser.add_argument('--host', dest='hostname', type=str, default='arcadia.f5ase.net',
+                        help='host header to send (default: same as origin)')
+    parser.add_argument('--ip', dest='ip', type=str, default='127.0.0.1',
+                        help='listen on IP (default: 127.0.0.1')
+    parser.add_argument('--proto', dest='protocol', type=str, default='http',
+                        help='protocol - either http or https (default: http')
+    parser.add_argument('--model', dest='load_my_model', type=str, default='detector_model_distilbert',
+                        help='Predictor model to use (default: detector_model_distilbert)')
+    args = parser.parse_args(argv)
+    return args
+args = parse_args()
+print(args)
 
 def proc(req):
  # loading preprocess and model file
         #features = pickle.load(open('/home/jupyter/Jupyter/notebook/ml_sqli_detector/predictor_distilbert/tf_model.preproc',
         #                    'rb'))
-        predictor = ktrain.load_predictor(load_my_model)
-        new_model = ktrain.get_predictor(predictor.model, predictor.preproc)
+        #predictor = ktrain.load_predictor(load_my_model)
+        #new_model = ktrain.get_predictor(predictor.model, predictor.preproc)
         #labels = ['benign', 'sqli', 'xss']
         path = urllib.parse.unquote(req)
         try:
@@ -40,6 +53,7 @@ def proc(req):
             text = path
         #list = text.split()
         #preproc_text = features.preprocess(list)
+        new_model = ml_model()
         result = new_model.predict(text)
         #label = labels[result[0].argmax(axis=0)]
         #score = ('{:.2f}'.format(round(np.max(result[0]), 2)*100))
@@ -56,14 +70,14 @@ class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self, body=True):
         result = proc(self.path)
-        if result != "benign":
+        if result != "normal":
             self.send_response(403, "Payload is %s" % result)
             #self.send_header("Set-Cookie", "foo=bar")
             self.end_headers()
         else:
             sent = False
             try:
-                url = '{}://{}{}'.format(protocol, origin, self.path)
+                url = '{}://{}{}'.format(args.protocol, args.origin, self.path)
                 req_header = self.parse_headers()
 
                 #print(req_header)
@@ -92,7 +106,7 @@ class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
         else:
             sent = False
             try:
-                url = '{}://{}{}'.format(protocol, hostname, self.path)
+                url = '{}://{}{}'.format(args.protocol, args.origin, self.path)
                 content_len = int(self.headers.get('Content-Length', 0))
                 post_body = self.rfile.read(content_len)
                 req_header = self.parse_headers()
@@ -130,23 +144,6 @@ class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_header('Content-Length', len(resp.content))
         self.end_headers()
 
-def parse_args(argv=sys.argv[1:]):
-    parser = argparse.ArgumentParser(description='Proxy HTTP requests')
-    parser.add_argument('--port', dest='port', type=int, default=8081,
-                        help='serve HTTP requests on specified port (default: random)')
-    parser.add_argument('--origin', dest='origin', type=str, default='arcadia.f5ase.net',
-                        help='hostname of the origin (default: arcadia.f5ase.net)')
-    parser.add_argument('--host', dest='hostname', type=str, default=origin,
-                        help='host header to send (default: same as origin)')
-    parser.add_argument('--ip', dest='ip', type=str, default='127.0.0.1',
-                        help='listen on IP (default: 127.0.0.1')
-    parser.add_argument('--proto', dest='protocol', type=str, default='http',
-                        help='protocol - either http or https (default: http')
-    parser.add_argument('--model', dest='load_my_model', type=str, default='detector_model_distilbert',
-                        help='Predictor model to use (default: detector_model_distilbert)')
-    args = parser.parse_args(argv)
-    return args
-
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
         """Handle requests in a separate thread."""
@@ -160,7 +157,10 @@ def main(argv=sys.argv[1:]):
     protocol = args.protocol
     httpd = ThreadedHTTPServer(server_address, ProxyHTTPRequestHandler)
     httpd.serve_forever()
-
+def ml_model():
+    predictor = ktrain.load_predictor(args.load_my_model)
+    new_model = ktrain.get_predictor(predictor.model, predictor.preproc)
+    return new_model
 if __name__ == '__main__':
     try:
         main()
